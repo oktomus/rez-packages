e786e8ff06f867a7ecdd6647bbfebe48ffe2181e
Kevin Masson <contact.kevinmasson@gmail.com>
2018-10-12 10:58:34 +0200

    Update gcc package file

--- gcc/4.8.5/package.py
+++ gcc/4.8.5/package.py
@@ -9,7 +9,10 @@ tools = ['gcc', 'g++', 'gcc-4.8', 'g++-4.8']
 variants = [['platform-linux', 'arch-x86_64', 'os-Arch-rolling']]
 
 def commands():
-    env.PATH.append('{this.root}/bin')
-    env.CC.set("{root}/bin/gcc")
-    env.CXX.set("{root}/bin/g++")
+    env.PATH.append("{root}/bin")
+    env.CC.set("{root}/bin/gcc-4.8")
+    env.CXX.set("{root}/bin/g++-4.8")
+    
+    if building:
+        env.LD_LIBRARY_PATH.append("{root}/lib")
 
